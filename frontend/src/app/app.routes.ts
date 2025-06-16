7224433c155c43e4e76618150c5965084bc8bacb
gustavogss <gustavogss.dev@gmail.com>
2025-06-16 02:05:37 -0300

    fix: fix project

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { SignupComponent } from './page/signup/signup.component';
 
 export const routes: Routes = [
   {
+    path:"",
+    component: LoginComponent
+  },
+  {
     path:"login",
     component: LoginComponent
   },
