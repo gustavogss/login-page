69ab4876ebbb2446644c28b9706a1bb92c41d4bf
gustavogss <gustavogss.dev@gmail.com>
2025-06-16 01:34:02 -0300

    feat: signup page created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
+import { SignupComponent } from './page/signup/signup.component';
 
 export const routes: Routes = [
   {
     path:"login",
     component: LoginComponent
+  },
+  {
+    path:"signup",
+    component: SignupComponent
   }
 ];
